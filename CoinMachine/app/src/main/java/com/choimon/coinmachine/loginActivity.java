6b7e8f9c697e89a39076c1ffbbf7d8da6899edc9
ChoiHyuongWuk <1993chw@gmail.com>
2021-06-17 01:47:14 +0900

    버전 업데이트
    V1.0.0 버전 업데이트.
    프로젝트 파일 개설

---
+++ CoinMachine/app/src/main/java/com/choimon/coinmachine/loginActivity.java
@@ -0,0 +1,17 @@
+package com.choimon.coinmachine;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class loginActivity {
+    private Button sign_up; // 회원가입 버튼
+    private EditText id_Input; // 아이디 입력
+    private EditText Pass_Input; // 비밀번호 입력
+    private TextView id_Seach; //아이디 찾기
+    private TextView Pass_Seach; //비밀번호 찾기
+
+    private final void login_action() {
+
+    }
+}
